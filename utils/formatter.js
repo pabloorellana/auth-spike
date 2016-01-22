0a49b6caf4314dc772afe4951f68db27d888fa57
pablo orellana <pablo.orellana.torrez@gmail.com>
2016-01-22 17:35:57 -0400

    Adding error response handlers

--- utils/formatter.js
+++ utils/formatter.js
@@ -1,4 +1,36 @@
-exports.excludeProperties = function (obj, params) {
+'use strict';
+
+var statusCodesHandlers = {
+    500: function (params) {
+        return buildError({
+            'status': '500',
+            'title' : params.title || 'Internal Server Error',
+            'detail': params.detail || 'Something went wrong.'
+        });
+    },
+    404: function (params) {
+        return buildError({
+            'status': '404',
+            'title':  params.title || 'Not Found',
+            'detail': params.detail || ''
+        });
+    },
+    409: function (params) {
+        return buildError({
+            'status': '409',
+            'title' : params.title || 'Conflict',
+            'detail': params.detail || ''
+        });
+    }
+};
+
+function buildError (errors) {
+    return {
+        'errors': [errors]
+    }
+};
+
+function excludeProperties (obj, params) {
     obj = JSON.parse(JSON.stringify(obj));
 
     for (var key in params) {
@@ -9,3 +41,16 @@ exports.excludeProperties = function (obj, params) {
 
     return obj;
 }
+
+function formatErrorResponse (req, res) {
+
+    var handler = statusCodesHandlers[res.errorInfo.status];
+
+    if (handler) {
+        return res.status(res.errorInfo.status).send(handler(res.errorInfo));
+    }
+
+    return res.status(500).send(statusCodesHandlers[500]({}));
+}
+
+module.exports = { excludeProperties, formatErrorResponse };
