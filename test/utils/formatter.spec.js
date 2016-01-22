98417bf79f07daa6ead6f3c2b24bc23744b43769
pablo orellana <pablo.orellana.torrez@gmail.com>
2016-01-22 19:08:05 -0400

    Adding unit tests for formatErrorResponse

--- test/utils/formatter.spec.js
+++ test/utils/formatter.spec.js
@@ -1,14 +1,25 @@
 'use strict';
 
 var assert = require('chai').assert,
+    sinon = require('sinon'),
     formatter = require('../../utils/formatter.js');
 
 var userMock = {
-    username: 'USER1',
-    email: 'user@user.com',
-    password: 'myPassword',
-    secret: 'mySecret'
-};
+        username: 'USER1',
+        email: 'user@user.com',
+        password: 'myPassword',
+        secret: 'mySecret'
+    },
+    res = {
+        status: function () { return this; },
+        send: function () {}
+    },
+
+    statusSpy,
+
+    sendSpy,
+
+    sandbox;
 
 describe('formatter', function () {
 
@@ -62,6 +73,83 @@ describe('formatter', function () {
             assert.property(result, 'secret');
         });
 
+    });
+
+    describe('formatErrorResponse()', function () {
+
+        beforeEach(function () {
+            sandbox = sinon.sandbox.create();
+
+            statusSpy = sandbox.spy(res, 'status');
+
+            sendSpy = sandbox.spy(res, 'send');
+        });
+
+        afterEach(function () {
+            sandbox.restore();
+        });
+
+        it('should create an 404 "Not Found" error message', function () {
+            res.errorInfo = {
+                status: 404
+            };
+
+            formatter.formatErrorResponse({ }, res);
+
+            assert(statusSpy.calledOnce);
+            assert(statusSpy.calledWith(404));
+            assert(sendSpy.calledWith({
+                errors: [
+                    {
+                        'status': res.errorInfo.status.toString(),
+                        'title':  'Not Found',
+                        'detail': ''
+                    }
+                ]
+            }));
+        });
+
+        it('should create an 409 "Custom Conflic" error message', function () {
+            res.errorInfo = {
+                status: 409,
+                title: 'Custom Conflic',
+                detail: 'This a custom detail message'
+            };
+
+            formatter.formatErrorResponse({ }, res);
+
+            assert(statusSpy.calledOnce);
+            assert(statusSpy.calledWith(409));
+            assert(sendSpy.calledWith({
+                errors: [
+                    {
+                        'status': res.errorInfo.status.toString(),
+                        'title': res.errorInfo.title,
+                        'detail': res.errorInfo.detail
+                    }
+                ]
+            }));
+        });
+
+        it('should return standard 500 error when there is no handler for the specified status', function () {
+            res.errorInfo = {
+                status: 600
+            };
+
+            formatter.formatErrorResponse({ }, res);
+
+            assert(statusSpy.calledOnce);
+            assert(statusSpy.calledWith(500));
+            assert(sendSpy.calledWith({
+                errors: [
+                    {
+                        'status': '500',
+                        'title' : 'Internal Server Error',
+                        'detail': 'Something went wrong.'
+                    }
+                ]
+            }));
+        });
     });
 
 });
